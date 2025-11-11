852050c345c337861567efacbac6a00221827d6a
Kaden McEntire <122327533+kdnmcntr@users.noreply.github.com>
2025-11-10 19:22:05 -0700

    Made program functional
    The program now functions properly.

---
+++ src/encryptor.rs
@@ -0,0 +1,125 @@
+use std::io;
+// use std::env;
+use std::fs::File;
+use std::thread;
+use std::sync::Arc;
+//use std::time::{Instant, Duration};
+use rand::{SeedableRng, RngCore};
+use rand_chacha::ChaCha20Rng;
+use io_at::{ReadAt, WriteAt};
+//use indicatif::{ProgressBar, ProgressStyle};
+
+fn hash_string(in_string: &str) -> u64 {
+
+    let mut hash_value: u64 = 0;
+    let prime = 131;
+
+    for c in in_string.bytes() {
+        if hash_value > 9_223_372_036_854_775_808 {
+            hash_value /= 2;
+        }
+        hash_value += (c as u16 * prime) as u64;
+    }
+
+    hash_value
+}
+
+pub struct Encryptor{
+    input_file: String,
+    output_file: String,
+    keyword: String,
+}
+
+impl Encryptor {
+    pub fn new(input_file: String, output_file: String, keyword: String) -> Self {
+        Self {input_file, output_file, keyword}
+    }
+
+    pub fn encrypt_file(&self) -> io::Result<()> {
+
+        let source: &str = &self.input_file;
+        let keyword: &str = &self.keyword;
+        let output: &str = &self.output_file;
+
+        let source_file = File::options().read(true).open(source)?;
+        let output_file = File::create(output)?;
+
+        let file_size = source_file.metadata()?.len();
+
+        output_file.set_len(file_size)?;
+
+        let seed: u64 = hash_string(keyword);
+        let mut rng = ChaCha20Rng::seed_from_u64(seed);
+
+        let segment_size: u64 = 1048576; // 1 MB 1048576, 1 KB 1024
+        let number_of_segments: u64 = file_size / segment_size + 1;
+
+        let mut handles = vec![];
+        let mut new_seeds = vec![];
+
+        let source_file = Arc::new(source_file);
+        let output_file = Arc::new(output_file);
+
+        for i in 0..number_of_segments {
+            new_seeds.push(rng.next_u64());
+            let new_seeds_clone = new_seeds.clone();
+
+            let source_file_clone = Arc::clone(&source_file);
+            let output_file_clone = Arc::clone(&output_file);
+
+            let handle = thread::spawn(move || {
+
+
+                let mut new_rng = ChaCha20Rng::seed_from_u64(new_seeds_clone[i as usize]);
+
+                let mut buffer = [0u8; 8];
+                for j in 0..(segment_size / 8) {
+                    let num_bytes_read = source_file_clone.read_at(&mut buffer, (segment_size * i) + (j * 8)).unwrap(); // read 8 bytes from the input file, store in buffer
+
+                    if num_bytes_read == 0 {
+                        break;
+                    }
+
+                    let read_data = u64::from_be_bytes(buffer); // convert buffer of 8 bytes into u64
+                    let rand_data = new_rng.next_u64(); // generate a new u64
+                    let new_data = read_data ^ rand_data; // xor the two u64s
+                    buffer = new_data.to_be_bytes(); // convert resulting u64 back into array of 8 bytes
+                    output_file_clone.as_ref().write_at(&buffer, (segment_size * i) + (j * 8)).unwrap(); // write buffer to the output file
+                    buffer = [0u8; 8]; // reset buffer
+                }
+
+            });
+
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        Ok(())
+
+    }
+
+}
+
+
+// fn format_time(time: Duration) -> String {
+//     let mut secs = time.as_secs();
+//     if secs < 1 {
+//         return String::from(time.as_millis().to_string() + " milliseconds");
+//     } else if secs < 60 {
+//         return String::from(secs.to_string() + " seconds");
+//     } else {
+//         let mut mins = secs / 60;
+//         secs = secs % 60;
+//
+//         if mins < 60 {
+//             return String::from(mins.to_string() + " minutes, " + &secs.to_string() + " seconds");
+//         } else {
+//             let hours = mins / 60;
+//             mins = mins % 60;
+//             return String::from(hours.to_string() + " hours, " + &mins.to_string() + " minutes," + &secs.to_string() + " seconds");
+//         }
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,159 @@
-fn main() {
-    println!("Hello, world!");
+mod encryptor;
+
+use std::path::PathBuf;
+use iced::{widget::{button, column, text, row, horizontal_rule, text_input}, window, Element, Task, Length::Fill, Border, Shadow, executor, Theme, Settings, Application};
+use iced::font::load;
+use rfd;
+use rfd::FileHandle;
+
+fn main() -> iced::Result {
+
+    iced::application("Keyword Encryptor", App::update, App::view).run()
+
+}
+
+#[derive(Debug)]
+struct App {
+    input_file: String,
+    output_file: String,
+    keyword: String,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            input_file: String::new(),
+            output_file: String::new(),
+            keyword: String::new(),
+        }
+    }
+}
+
+impl App {
+
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::Exit => window::get_latest().and_then(window::close),
+            Message::Encrypt => {
+                encryptor::Encryptor::new(self.input_file.clone(), self.output_file.clone(), self.keyword.clone()).encrypt_file().unwrap();
+                Task::none()
+            },
+            Message::Keyword_Changed(keywd) => {
+                self.keyword = keywd;
+                Task::none()
+            },
+            Message::InputChanged(file_name) => {
+                self.input_file = file_name;
+                Task::none()
+            },
+            Message::OutputChanged(output_file) => {
+                self.output_file = output_file;
+                Task::none()
+            },
+            Message::FileCancelled => {
+                Task::none()
+            },
+            Message::OpenFile => {
+                open_file()
+            },
+            Message::SaveFile => {
+                save_file()
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        let go_button = if self.is_ready() {
+            button(text("Go")).on_press(Message::Encrypt)
+        } else {
+            button(text("Go"))
+        };
+
+        let selected_input_file: String = if self.input_file.is_empty() {String::from("(No file selected)")} else {self.input_file.clone()};
+        let selected_output_file: String = if self.output_file.is_empty() {String::from("(No file selected)")} else {self.output_file.clone()};
+
+        let mut content = column![
+        row![
+            text("Source file: "),
+            text(selected_input_file),
+            button("Select File").on_press(Message::OpenFile),
+        ],
+        row![
+            text("Output file: "),
+            text(selected_output_file),
+            button("Select File").on_press(Message::SaveFile),
+        ],
+        row![
+            text("Password"),
+            text_input("Type password here", &self.keyword)
+                .on_input(|s| Message::Keyword_Changed(s)),
+        ],
+        row![
+            button(text("Cancel")).on_press(Message::Exit),
+            go_button,
+        ]
+    ];
+
+        content.into()
+    }
+
+    fn is_ready(&self) -> bool {
+        !self.input_file.is_empty() && !self.output_file.is_empty() && !self.keyword.is_empty()
+    }
+
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Exit,
+    Encrypt,
+    Keyword_Changed(String),
+    InputChanged(String),
+    OutputChanged(String),
+    FileCancelled,
+    OpenFile,
+    SaveFile,
+}
+
+
+
+fn open_file() -> Task<Message> {
+    Task::future(
+        rfd::AsyncFileDialog::new()
+            .pick_file(),
+    ).then(|handle| {
+        match handle {
+            Some(file_handle) => Task::perform(load_input_file(file_handle), |result| Message::InputChanged(result)),
+            None => Task::done(Message::FileCancelled),
+        }
+    })
+
+}
+
+async fn load_input_file(file_handle: FileHandle) -> String {
+    match Some(file_handle.path()) {
+        Some(path) => path.to_string_lossy().to_string(),
+        None => String::new(), // fallback if path is unavailable
+    }
+}
+
+
+fn save_file() -> Task<Message> {
+    Task::future(
+        rfd::AsyncFileDialog::new()
+            .set_file_name("build info.txt")
+            .save_file(),
+    ).then(|handle| {
+        match handle{
+            Some(file_handle) => Task::perform(save_output_file(file_handle), |result| Message::OutputChanged(result)),
+            None => Task::done(Message::FileCancelled),
+        }
+    })
+}
+
+async fn save_output_file(file_handle: FileHandle) -> String {
+    match Some(file_handle.path()) {
+        Some(path) => path.to_string_lossy().to_string(),
+        None => String::new(), // fallback if path is unavailable
+    }
 }
